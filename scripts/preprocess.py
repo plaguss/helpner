83eb67668b080d7b23a2d615f5d484574382eef5
agustin <agustin.piqueres@gmail.com>
2022-12-26 21:08:00 +0100

    feat: controls bad annotations for the time being

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -3,8 +3,6 @@
 Copied from the spacy tutorials:
 https://spacy.io/usage/training#training-data
 https://github.com/explosion/projects/blob/v3/tutorials/ner_fashion_brands/scripts/preprocess.py
-
-python scripts/preprocess.py ./assets/v0.0.1/dataset/dataset.jsonl ./assets/v0.0.1/dataset/dataset.spacy
 """
 
 import typer
@@ -20,14 +18,26 @@ def main(
 ):
     nlp = spacy.blank("en")
     doc_bin = DocBin()
+    i = 0
     for eg in srsly.read_jsonl(input_path):
 
         doc = nlp(eg["message"])
+
+        if doc is None:
+            raise ValueError("??")
         ents = []
         for label, start, end in eg["annotations"]:
             ents.append(doc.char_span(start, end, label=label))
 
+        if len(ents) > 0:
+            try:
+                doc.ents = ents
+            except TypeError:
+                print(f"Message i={i} can't be created, review cli_help_maker generator.")
+                continue
+
         doc_bin.add(doc)
+        i += 1
 
     doc_bin.to_disk(output_path)
     print(f"Processed {len(doc_bin)} documents: {output_path.name}")
