fc9e57656dadfd7fc3eba58c5f63980e36a477d8
agustin <agustin.piqueres@gmail.com>
2023-01-28 19:59:29 +0100

    feat: download exposed as a function to simplify calling it from the cli

--- helpner/download.py
+++ helpner/download.py
@@ -12,26 +12,42 @@ import json
 import subprocess
 import sys
 from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
 
 from packaging.version import parse
-
-url = r"https://github.com/plaguss/helpner-core/releases/download/v0.1.3/en_helpner_core-0.1.3-py3-none-any.whl"
-
-endpoint = r"https://api.github.com/repos/plaguss/helpner-core/releases"
+RELEASES_URL = r"https://github.com/plaguss/helpner-core/releases"
+RELEASES_ENDPOINT = r"https://api.github.com/repos/plaguss/helpner-core/releases"
 
 
-with urlopen(endpoint) as response:
-    body = response.read()
-    releases = json.loads(body)
+def download_model() -> None:
+    """Downloads the lastest model from `helpner-core` releases.
 
-# Get the last uploaded release
-versions = [parse(r["name"].split("-")[-1]) for r in releases]
-last_release = versions.index(max(versions))
-assets = releases[last_release]["assets"]
-# Find the wheel package
-whl = [
-    a["browser_download_url"]
-    for a in assets
-    if a["browser_download_url"].endswith("whl")
-][0]
-subprocess.run([sys.executable, "-m", "pip", "install", whl], check=True)
+    Raises:
+        HTTPError: Raised if calling urlopen fails.
+            If that occurs, a url pointing to the models would appear.
+    """
+    try:
+        with urlopen(RELEASES_ENDPOINT) as response:
+            body = response.read()
+    except (URLError, HTTPError) as e:
+        raise HTTPError(f"Something failed, the model should be installed from: {RELEASES_URL}") from e
+    else:
+        releases = json.loads(body)
+
+    # Get the last uploaded release
+    versions = [parse(r["name"].split("-")[-1]) for r in releases]
+    last_release = versions.index(max(versions))
+    assets = releases[last_release]["assets"]
+    # Find the wheel package instead of the sdist.
+    whl = [
+        a["browser_download_url"]
+        for a in assets
+        if a["browser_download_url"].endswith("whl")
+    ][0]
+    # An example browser_download_url path would be:
+    # https://github.com/plaguss/helpner-core/releases/download/v0.1.3/en_helpner_core-0.1.3-py3-none-any.whl
+    subprocess.run([sys.executable, "-m", "pip", "install", whl], check=True)
+
+
+if __name__ == "__main__":
+    download_model()
