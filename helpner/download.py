b24720b41b2b6e587ec4ede3469bd84ec9427ae6
agustin <agustin.piqueres@gmail.com>
2023-02-10 18:52:12 +0100

    feat: split functions to simplify testing

--- helpner/download.py
+++ helpner/download.py
@@ -6,6 +6,7 @@ and extracts the wheel from those assets.
 import json
 import subprocess
 import sys
+from typing import Any
 from urllib.error import HTTPError, URLError
 from urllib.request import urlopen
 
@@ -15,7 +16,24 @@ RELEASES_URL = r"https://github.com/plaguss/helpner-core/releases"
 RELEASES_ENDPOINT = r"https://api.github.com/repos/plaguss/helpner-core/releases"
 
 
-def download_model() -> None:
+def _get_wheel(releases: Any) -> str:
+    """Parses the response from github api to obtain
+    the most updated wheel.
+    """
+    # Get the last uploaded release
+    versions = [parse(r["name"].split("-")[-1]) for r in releases]
+    last_release = versions.index(max(versions))
+    assets = releases[last_release]["assets"]
+    # Find the wheel package instead of the sdist.
+    whl = [
+        a["browser_download_url"]
+        for a in assets
+        if a["browser_download_url"].endswith("whl")
+    ][0]
+    return whl
+
+
+def download_model() -> None:  # pragma: no cover
     """Downloads the lastest model from `helpner-core` releases.
 
     Raises:
@@ -32,20 +50,11 @@ def download_model() -> None:
     else:
         releases = json.loads(body)
 
-    # Get the last uploaded release
-    versions = [parse(r["name"].split("-")[-1]) for r in releases]
-    last_release = versions.index(max(versions))
-    assets = releases[last_release]["assets"]
-    # Find the wheel package instead of the sdist.
-    whl = [
-        a["browser_download_url"]
-        for a in assets
-        if a["browser_download_url"].endswith("whl")
-    ][0]
+    whl = _get_wheel(releases)
     # An example browser_download_url path would be:
     # https://github.com/plaguss/helpner-core/releases/download/v0.1.3/en_helpner_core-0.1.3-py3-none-any.whl
     subprocess.run([sys.executable, "-m", "pip", "install", whl], check=True)
 
 
 if __name__ == "__main__":
-    download_model()
+    download_model()  # pragma: no cover
